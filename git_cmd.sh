e0c8f3ad2561fc5bcd990c302c0fdb085b7e980a
kazushi <kimoto@okayama-u.ac.jp>
2021-02-02 17:17:29 +0900

    1st commit

---
+++ git_cmd.sh
@@ -0,0 +1,17 @@
+# Readme 
+git add README.txt
+# Git commands
+git add git_cmd.sh
+# python, C/C++ source
+git add Src/*.cpp
+git add Src/*.h
+git add Src/makefile
+git add Tex/*.tex
+git add Tex/*.makefile
+git add Tex/*.svgz
+git add Tex/*.cls
+
+git commit 
+
+git push -u origin master
+
